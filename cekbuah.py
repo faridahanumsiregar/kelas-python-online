c4bd73fc26e95c30c0bd550438ce0198b431734b
Vicky Dasta <7861544+vck@users.noreply.github.com>
2020-04-11 14:30:34 +0700

    Create cekbuah.py

---
+++ cekbuah.py
@@ -0,0 +1,8 @@
+keranjang1 = ["melon", "durian", "manggis", "duku", "mangga"]
+cek = input("> nama buah: ")
+
+if cek in keranjang1:
+    print(cek, "ada di keranjang")
+
+else:
+    print(cek, "tidak ada di keranjang")
