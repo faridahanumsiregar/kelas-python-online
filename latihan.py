84217a15e0bc6e872c183f0cc9ea0d613adb2704
Vicky Dasta <7861544+vck@users.noreply.github.com>
2020-04-13 22:34:14 +0700

    Create latihan.py

---
+++ latihan.py
@@ -0,0 +1,21 @@
+# string
+# fungsi
+# list
+
+nama = input("masukkan nama anda> ")
+banyak_karakter = len(nama)
+
+print("banyak karakter dari nama anda", nama, "adalah", banyak_karakter)
+
+# tujuan program
+# 1. menghitung banyak karakter dari nama
+# > count how many character in string in Python
+# 2. menampilkan karakter per baris
+
+def kapital(nama):
+	return nama.upper()
+
+def add(a, b):
+	return str(a)+str(b)
+
+print(add(1, "a")) 
